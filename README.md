b082189ccb45695c719711c8dd88016cfba31da1
Benjamin Land <benland100@gmail.com>
2021-01-21 14:15:22 -0500

    Add GPLv3 license

---
+++ README.md
@@ -0,0 +1,15 @@
+# L2
+
+This is a python version of the [L language](https://github.com/benland100/L) 
+with a few changes to function naming schemes. Like L, L2 is an interpreted
+LISP-like language, with an emphasis on having a very simple interpreter in the
+host lanugage (Python), with most language functionality implemented in L2 
+itself. 
+
+L2 aims to be a software implementation of a machine, or hardware, that is
+capable of executing L2 code directly. This project aims to explore simple 
+Turing-complete machines that strike a balance of "easy to program" (as seen
+in non Neumann machines) and "simple to build" (as seen in Rule 110 
+automatons), which are competing criteria in programmable machines.
+
+Copyright 2021 by Benjamin J. Land (a.k.a. BenLand100)
--- l2/binmap.py
+++ l2/binmap.py
@@ -1,3 +1,18 @@
+#  This file is part of L2.
+#
+#  L2 is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  L2 is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with L2.  If not, see <https://www.gnu.org/licenses/>.
+
 from .cell import Cell
 
 '''Python logic for creating a binary tree of (Symbol) keys that map to values out of Cells.'''
